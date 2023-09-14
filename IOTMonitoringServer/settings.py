9a68a88ec47def5bf542502e13e3a35df35b4869
gcastrot82 <gcastrot82@hotmail.com>
2023-09-13 22:14:35 -0500

    Actualizacion de IP 4

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "3.89.231.207"]
+ALLOWED_HOSTS = ["localhost", "107.21.177.71"]
 
 
 # Application definition
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "3.88.107.49",  # Dirección IP de la base de datos
+        "HOST": "34.201.147.138",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -155,7 +155,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "54.87.38.50"
+MQTT_HOST = "75.101.218.243"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
