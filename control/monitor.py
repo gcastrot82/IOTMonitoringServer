4b70d0f1860a37c90b32a20f6d785de5cf74828d
gcastrot82 <gcastrot82@hotmail.com>
2023-09-16 11:40:02 -0500

    Actualizacion de IP 23

--- control/monitor.py
+++ control/monitor.py
@@ -57,7 +57,7 @@ def analyze_data():
 #aggregation = data.annotate(check_value=Avg('avg_value')) \
     #data = Data.objects.filter(base_time__gte=datetime.now() - timedelta(hours=1)) 
     data = Data.objects.filter(base_time__gte=datetime.now() - timedelta(minutes=1)) 
-    aggregation = data.annotate(min_value=Min('avg_value')) \
+    aggregation = data.annotate(min_value=Min('measurement_id')) \
         .select_related('station', 'measurement') \
         .select_related('station__user', 'station__location') \
         .select_related('station__location__city', 'station__location__state',
